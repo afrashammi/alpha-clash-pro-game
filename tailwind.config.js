82dc1ff4feca83f8fa784894059b93e07be74ddd
afrashammi <shammiafra@gmail.com>
2024-04-03 11:20:03 +0800

    project set up

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind getstarted tailwind cli
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
